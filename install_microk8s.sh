fda45a9c08dbb9b6aa936e0e4e78db16c6e62553
Damian Orzepowski <damian.orzepowski@4chain.studio>
2023-04-24 15:07:21 +0200

    feat: enabke argocd

--- install_microk8s.sh
+++ install_microk8s.sh
@@ -6,5 +6,9 @@ sudo which snap || (sudo apt update && sudo apt install -y snapd)
 # install microk8s 1.27
 sudo snap install microk8s --classic --channel=1.27/stable
 
+# enable community addons so configuration update should go smoothly
+sudo microk8s enable community
+
 # apply configuration
-sudo snap set microk8s config="$(cat microk8s-config.yaml)"
+# ATTENTION - in case if some addons doen't load run update_microk8s.sh script again
+./update_microk8s.sh
