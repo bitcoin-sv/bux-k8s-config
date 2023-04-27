ca4f16b24250c1d40a007a2f05bb7ab539f48926
Damian Orzepowski <damian.orzepowski@4chain.studio>
2023-04-27 13:54:49 +0200

    feat(#5): handle only https

--- apply.sh
+++ apply.sh
@@ -4,6 +4,7 @@
 # For now add your file to the command
 
 sudo sudo microk8s.kubectl apply  -f ./devops/cert-manager/cluster-issuer.yml \
+                                  -f ./devops/traefik/https-redirect.yml \
                                   -f ./apps/pulse/deployment.yml \
                                   -f ./apps/pulse/service.yml \
-                                  -f ./apps/pulse/ingress.yml \
+                                  -f ./apps/pulse/ingress.yml
