9df173f8c002f8b7e72c1dd56159e164ed758202
Damian Orzepowski <damian.orzepowski@4chain.studio>
2023-04-27 12:06:19 +0200

    feat(#5): setup cluster manager

--- apply.sh
+++ apply.sh
@@ -3,7 +3,8 @@
 # Temporary solution for applying all k8s changes (should be replaced with argo cd)
 # For now add your file to the command
 
-sudo sudo microk8s.kubectl apply  -f ./devops/dashboard/ingress.yml \
+sudo sudo microk8s.kubectl apply  -f ./devops/cert-manager/cluster-issuer.yml \
+                                  -f ./devops/dashboard/ingress.yml \
                                   -f ./apps/pulse/deployment.yml \
                                   -f ./apps/pulse/service.yml \
                                   -f ./apps/pulse/ingress.yml \
