e3f8f9421ac465ca64f422b9e93eefc8c31a5f8a
Damian Orzepowski <damian.orzepowski@4chain.studio>
2023-04-28 14:54:39 +0200

    feat(#14): add environment variables to bux-console

--- apply.sh
+++ apply.sh
@@ -8,6 +8,7 @@ sudo sudo microk8s.kubectl apply  -f ./devops/cert-manager/cluster-issuer.yml \
                                   -f ./apps/pulse/deployment.yml \
                                   -f ./apps/pulse/service.yml \
                                   -f ./apps/pulse/ingress.yml \
+                                  -f ./apps/bux-console/environment.yml \
                                   -f ./apps/bux-console/deployment.yml \
                                   -f ./apps/bux-console/service.yml \
                                   -f ./apps/bux-console/ingress.yml \
