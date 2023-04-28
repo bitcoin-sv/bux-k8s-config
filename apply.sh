ec9d4daa7ba4468f4834412a3f0dcc9e8470b006
Damian Orzepowski <damian.orzepowski@4chain.studio>
2023-04-28 14:30:29 +0200

    feat(#14): deploy bux-console

--- apply.sh
+++ apply.sh
@@ -8,8 +8,11 @@ sudo sudo microk8s.kubectl apply  -f ./devops/cert-manager/cluster-issuer.yml \
                                   -f ./apps/pulse/deployment.yml \
                                   -f ./apps/pulse/service.yml \
                                   -f ./apps/pulse/ingress.yml \
+                                  -f ./apps/bux-console/deployment.yml \
+                                  -f ./apps/bux-console/service.yml \
+                                  -f ./apps/bux-console/ingress.yml \
                                   -f ./apps/postgres-environment.yml \
                                   -f ./apps/postgres-pv.yml \
                                   -f ./apps/postgres-pvc.yml \
                                   -f ./apps/postgres-deployment.yml \
-                                  -f ./apps/postgres-service.yml 
+                                  -f ./apps/postgres-service.yml
