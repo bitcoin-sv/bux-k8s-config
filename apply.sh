f977e6d15dd3ceece44434d64e140b3d68fb651b
wregulski <48433067+wregulski@users.noreply.github.com>
2023-05-02 14:28:07 +0200

    feat: storage postgres - update for mayastore (#33)

--- apply.sh
+++ apply.sh
@@ -7,4 +7,9 @@ sudo sudo microk8s.kubectl apply  -f ./devops/cert-manager/cluster-issuer.yml \
                                   -f ./devops/traefik/https-redirect.yml \
                                   -f ./apps/pulse/deployment.yml \
                                   -f ./apps/pulse/service.yml \
-                                  -f ./apps/pulse/ingress.yml
+                                  -f ./apps/pulse/ingress.yml \
+                                  -f ./apps/postgres-environment.yml \
+                                  -f ./apps/postgres-pv.yml \
+                                  -f ./apps/postgres-pvc.yml \
+                                  -f ./apps/postgres-deployment.yml \
+                                  -f ./apps/postgres-service.yml 
