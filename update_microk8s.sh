23d45263a5a6580208330fe4221badf84648871e
Damian Orzepowski <damian.orzepowski@4chain.studio>
2023-07-04 12:52:26 +0200

    fix(#48): fix pending status of ingress components

--- update_microk8s.sh
+++ update_microk8s.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Enable traefik and pass custom arguments to it: don't know how to do it with microk8s-config.yaml - set command is not working there
 sudo microk8s enable community
-sudo microk8s enable traefik --set="additionalArguments={--serverstransport.insecureskipverify=true}"
+sudo microk8s enable traefik --set="additionalArguments={--serverstransport.insecureskipverify=true,--providers.kubernetesingress.ingressendpoint.publishedservice=traefik/traefik}"
 
 
 sudo snap set microk8s config="$(cat microk8s-config.yaml)"
