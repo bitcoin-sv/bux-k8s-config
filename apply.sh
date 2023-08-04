cf7e2ea39dbf34b0c54de1857193d8572badc178
Darren Kellenschwiler <deggen@kschw.com>
2023-08-04 12:45:29 -0500

    update apply script
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- apply.sh
+++ apply.sh
@@ -3,23 +3,23 @@
 # Temporary solution for applying all k8s changes (should be replaced with argo cd)
 # For now add your file to the command
 
-sudo sudo microk8s.kubectl apply \
+sudo sudo microk8s kubectl apply \
   -f ./devops/cert-manager/cluster-issuer.yml \
   -f ./devops/traefik/https-redirect.yml \
   -f ./devops/argo-cd/ingress.yml;
 
-sudo sudo microk8s.kubectl apply \
+sudo microk8s kubectl apply \
   -f ./apps/pulse/deployment.yml \
   -f ./apps/pulse/service.yml \
   -f ./apps/pulse/ingress.yml;
 
-sudo sudo microk8s.kubectl apply \
+sudo microk8s kubectl apply \
   -f ./apps/bux-console/environment.yml \
   -f ./apps/bux-console/deployment.yml \
   -f ./apps/bux-console/service.yml \
   -f ./apps/bux-console/ingress.yml;
 
-sudo sudo microk8s.kubectl apply \
+sudo microk8s kubectl apply \
   -f ./apps/bux-server/postgres/environment.yml \
   -f ./apps/bux-server/postgres/pvc.yml \
   -f ./apps/bux-server/postgres/deployment.yml \
@@ -27,6 +27,7 @@ sudo sudo microk8s.kubectl apply \
   -f ./apps/bux-server/redis/pvc.yml \
   -f ./apps/bux-server/redis/deployment.yml \
   -f ./apps/bux-server/redis/service.yml;
+  
 sudo microk8s kubectl apply -k apps/bux-server/bux/development;
 
 sudo microk8s kubectl apply \
@@ -43,7 +44,7 @@ sudo microk8s kubectl apply \
   -f ./apps/bux-wallet/frontend/service.yml \
   -f ./apps/bux-wallet/frontend/deployment.yml;
 
-sudo sudo microk8s.kubectl -n argocd apply \
+sudo microk8s kubectl -n argocd apply \
       -f ./devops/argo-cd/repository-connector-secret.yml \
       -f ./apps/bux-console/argocd-def.yml \
       -f ./apps/pulse/argocd-def.yml \
