025eb34281e188db282d80defab02e641f82d3b4
Darren Kellenschwiler <d.kellenschwiler@bitcoinassociation.net>
2023-08-11 14:16:28 -0500

    update replacements in all

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
-export BUX_DOMAIN_NAME_TLD="$1"
-export BUX_HOST_IP="$2"
+# Check if at least one argument is provided
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <domain-name> <host-ip> <github-username>"
+    exit 1
+fi
 
-./replace_domain.sh
-./replace_host_ip.sh
+./replace_domain.sh $1
+./replace_host_ip.sh $2
+./replace_github_username.sh $3
 ./install_microk8s.sh
