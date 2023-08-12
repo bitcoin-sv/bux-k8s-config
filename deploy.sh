7946793d5a884d9e30d75ae36742ab2442ac07b7
Darren Kellenschwiler <deggen@kschw.com>
2023-08-12 13:20:59 -0500

    argocd password reveal
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 sudo sh apply.sh
 sudo sh update_microk8s.sh
 echo "Installation complete, please navigate to https://cd.your-domain.tld which should become availble within the next 20 minutes."
-echo "Argo CD password:\n"
-
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-
-echo "\n"
+argocdpswd=$(sudo microk8s kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) 
+printf "\nArgo CD username: admin\npassword: %s\n\n" $argocdpswd
