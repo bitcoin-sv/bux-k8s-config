65d37d2f18ace2d9f79b8a3faaae99684cce01c9
Darren Kellenschwiler <deggen@kschw.com>
2023-08-12 12:55:58 -0500

    deploy update with admin password to console
    Signed-off-by: Darren Kellenschwiler <deggen@kschw.com>

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
+sudo sh apply.sh
 sudo sh update_microk8s.sh
 echo "Installation complete, please navigate to https://cd.your-domain.tld which should become availble within the next 20 minutes."
+echo "Argo CD password:\n"
+
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+echo "\n"
